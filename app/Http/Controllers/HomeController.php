abeac32276b38c9436b80df71fc7143f1ab739a0
sdad0o <slwdsaw@gmail.com>
2025-02-09 15:34:19 +0300

    Intracting with requset - Input requset examples

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,16 +13,27 @@ class HomeController extends Controller
     {
 
         //--------- Requset path and method----------- 
-        // dd($request->path());  to show the path 
+        // dd($request->path());  //to show the path 
         // to cheack a condition 
         // dd($request->is('/'));
         // dd($request->routeIs('home'));
         // dd($request->url());
-        // dd($request->fullUrl()); with the all arameters
-        // dd($request->fullUrlWithoutQuery('age')); for the fillters
+        // dd($request->fullUrl()); //with the all arameters
+        // dd($request->fullUrlWithoutQuery('age')); //for the fillters
         // dd($request->method());
         // dd($request->isMethod('post'));
 
+        //--------- Requset Input ------------------- 
+        // dd($request->all());
+        // dd($request->collect()); //the same but it's collection type
+        // dd($request->input()); // return array  use POST
+        // dd($request->query()); // using the qurey parameter use GET
+        // dd($request->boolean('is_admin'));
+        // dd($request->date('date'));
+        // dd($request->only('name'));
+        // dd($request->except('age'));
+        // dd($request->only(['name','age']));
+        
         return view('welcome');
     }
 }
