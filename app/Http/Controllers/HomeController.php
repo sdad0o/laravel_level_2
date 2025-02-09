e4a38c0261e80076c2284f3f3e75b850b27bbf9e
sdad0o <slwdsaw@gmail.com>
2025-02-09 16:35:26 +0300

    Requset Input presence (has , filling , missing)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,7 +33,30 @@ class HomeController extends Controller
         // dd($request->only('name'));
         // dd($request->except('age'));
         // dd($request->only(['name','age']));
-        
+
+        //--------- Requset Input presence (has , filling , missing) ------- 
+        // dd($request->has('name'));
+        // dd($request->hasAny(['name','age'])); work with array has name -or- age
+        // $request->whenHas('name',function(){
+        //     dd('requstet  has name');
+        // },function(){
+        //     dd('requset dosent have name ');
+        // });
+        // dd($request->filled('age')); // is it have value -- shold be exist and have value
+        // dd($request->anyFilled(['name','age'])); // like the has any
+        // $request->whenFilled('name',function(){
+        //     dd('requset have filled');
+        // }, function(){
+        //     dd('requset have not  filled');
+        // });
+
+        // dd($request->missing('name')); //is the name deleted or not exisit -- it's the opposite of the has 
+        // $request->whenMissing('name', function () {
+        //     dd('requset have missing');
+        // }, function () {
+        //     dd('requset have not  missing');
+        // });
+
         return view('welcome');
     }
 }
