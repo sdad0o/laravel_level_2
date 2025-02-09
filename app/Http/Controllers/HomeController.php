75187be35b4332f9e3c3dfeda95644b1bb5a6daf
sdad0o <slwdsaw@gmail.com>
2025-02-09 15:18:01 +0300

    Intracting with requset path and method examples

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,18 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+
+        //--------- Requset path and method----------- 
+        // dd($request->path());  to show the path 
+        // to cheack a condition 
+        // dd($request->is('/'));
+        // dd($request->routeIs('home'));
+        // dd($request->url());
+        // dd($request->fullUrl()); with the all arameters
+        // dd($request->fullUrlWithoutQuery('age')); for the fillters
+        // dd($request->method());
+        // dd($request->isMethod('post'));
+
         return view('welcome');
     }
 }
